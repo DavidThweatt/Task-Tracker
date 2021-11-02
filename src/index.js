cb9c3297f126d84a1288d4a628ca691774095491
DavidThweatt <ddthweatt9@gmail.com>
2021-11-02 14:20:27 -0700

    created sort feture and form validation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const addBtn = document.getElementById("addBTN");
 const taskForm = document.getElementById("new-task-form");
+const form = document.getElementById("task-form");
 const submitBtn = document.getElementById("submitBTN");
 const taskName = document.getElementById("task-name");
 const taskInfo = document.getElementById("task-info");
 const currentDiv = document.getElementById("current-task");
 const deleteBTN = document.getElementById("deleteBTN");
 const doneBTN = document.getElementById("doneBTN");
+const taskLink = document.getElementById("task-link");
+const taskFinished = document.getElementById("finished-link");
 
 function handleForm(event) {
   event.preventDefault();
@@ -25,37 +28,65 @@ function displayTaskForm() {
 }
 
 submitBtn.addEventListener("click", () => {
-  const currentDiv = document.getElementById("current-task");
+  let messages = [];
 
-  const title = document.createElement("h3");
-  title.innerText += taskName.value;
-  title.classList.add("newTaskTitle");
+  if (taskName.value === "" || taskName.value == null) {
+    messages.push("name is required");
+  }
 
-  const InfoBox = document.createElement("p");
-  InfoBox.innerText += taskInfo.value;
+  if (taskInfo.value === "" || taskInfo.value == null) {
+    messages.push("info is required");
+  }
 
-  const doneBTN = document.createElement("button");
-  doneBTN.classList.add("doneBTN");
-  doneBTN.innerText += "Done";
+  if (messages.length > 0) {
+    alert("Field Required");
+  } else {
+    const currentDiv = document.getElementById("current-task");
 
-  const deleteBTN = document.createElement("button");
-  deleteBTN.classList.add("deleteBTN");
-  deleteBTN.innerText += "Delete";
+    const title = document.createElement("h3");
+    title.innerText += taskName.value;
+    title.classList.add("newTaskTitle");
 
-  const newCurrentDiv = document.createElement("div");
-  newCurrentDiv.classList.add("newCurrentDiv");
+    const InfoBox = document.createElement("p");
+    InfoBox.innerText += taskInfo.value;
 
-  newCurrentDiv.appendChild(title);
-  newCurrentDiv.appendChild(InfoBox);
-  newCurrentDiv.appendChild(doneBTN);
-  newCurrentDiv.appendChild(deleteBTN);
+    const doneBTN = document.createElement("button");
+    doneBTN.classList.add("doneBTN");
+    doneBTN.innerText += "Done";
 
-  currentDiv.appendChild(newCurrentDiv);
-  doneBTN.addEventListener("click", () => {
-    newCurrentDiv.style.backgroundColor = "green";
-  });
+    const deleteBTN = document.createElement("button");
+    deleteBTN.classList.add("deleteBTN");
+    deleteBTN.innerText += "Delete";
 
-  deleteBTN.addEventListener("click", () => {
-    newCurrentDiv.remove();
-  });
+    const newCurrentDiv = document.createElement("div");
+    newCurrentDiv.classList.add("newCurrentDiv");
+
+    taskName.value = "";
+    taskInfo.value = "";
+
+    newCurrentDiv.appendChild(title);
+    newCurrentDiv.appendChild(InfoBox);
+    newCurrentDiv.appendChild(doneBTN);
+    newCurrentDiv.appendChild(deleteBTN);
+
+    currentDiv.appendChild(newCurrentDiv);
+    doneBTN.addEventListener("click", () => {
+      newCurrentDiv.style.backgroundColor = "green";
+    });
+
+    deleteBTN.addEventListener("click", () => {
+      newCurrentDiv.remove();
+    });
+
+    taskLink.addEventListener("click", () => {
+      taskForm.style.display = "none";
+      newCurrentDiv.style.display = "flex";
+    });
+
+    taskFinished.addEventListener("click", () => {
+      if (newCurrentDiv.style.backgroundColor !== "green") {
+        newCurrentDiv.style.display = "none";
+      }
+    });
+  }
 });
